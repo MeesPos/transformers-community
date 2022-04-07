146cd4087304b79f2f5cecd4aec8739bb7f21a20
MeesPos <meespostma_@hotmail.com>
2022-04-07 15:20:20 +0200

    Added birth_date to save user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,13 +24,16 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
+            'birth_date' => ['required', 'date'],
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
+        return User::query()
+            ->create([
+                'name' => $input['name'],
+                'email' => $input['email'],
+                'birth_date' => $input['birth_date'],
+                'password' => Hash::make($input['password']),
         ]);
     }
 }
