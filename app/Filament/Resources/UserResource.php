e56a855837adb178bc41f56126ed0dc2d3934e10
MeesPos <meespostma_@hotmail.com>
2022-04-10 13:29:58 +0200

    Removed view from User resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -58,7 +58,6 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
-            'view' => Pages\ViewUser::route('/view'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
