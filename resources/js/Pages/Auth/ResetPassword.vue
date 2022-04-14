c2a6fa90591a0c7782023cee2770236bef0f5d86
MeesPos <meespostma_@hotmail.com>
2022-04-14 16:35:58 +0200

    Added reset password styling

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,83 +1,118 @@
-<script setup>
-import { Head, useForm } from '@inertiajs/inertia-vue3';
+<script>
+import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue';
 import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue';
 import JetButton from '@/Jetstream/Button.vue';
 import JetInput from '@/Jetstream/Input.vue';
 import JetLabel from '@/Jetstream/Label.vue';
 import JetValidationErrors from '@/Jetstream/ValidationErrors.vue';
+import AuthLayout from "../../Layouts/AuthLayout";
 
-const props = defineProps({
-    email: String,
-    token: String,
-});
-
-const form = useForm({
-    token: props.token,
-    email: props.email,
-    password: '',
-    password_confirmation: '',
-});
-
-const submit = () => {
-    form.post(route('password.update'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
-};
+export default {
+    props: {
+        email: String,
+        token: String
+    },
+    components: {
+        Head,
+        Link,
+        JetAuthenticationCard,
+        JetAuthenticationCardLogo,
+        JetButton,
+        JetInput,
+        JetLabel,
+        JetValidationErrors
+    },
+    data(props) {
+        return {
+            form: useForm({
+                token: props.token,
+                email: props.email,
+                password: '',
+                password_confirmation: '',
+            })
+        }
+    },
+    methods: {
+        submit() {
+            this.form.post(this.route('password.update'), {
+                onFinish: () => this.form.reset('password', 'password_confirmation'),
+            });
+        }
+    },
+    layout: AuthLayout
+}
 </script>
 
 <template>
-    <Head title="Reset Password" />
+    <div class="w-[80%] mx-auto sm:ml-0">
+        <h2 class="text-6xl text-brand-purple">Wachtwoord veranderen</h2>
 
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
+        <p class="mt-10">Wachtwoord vergeten, geen probleem. Vul hier onder het e-mail adres in waar al eerder mee heeft in gelogd. </p>
 
-        <JetValidationErrors class="mb-4" />
+        <JetValidationErrors class="mt-6" />
 
         <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="email" value="Email" />
-                <JetInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
-            </div>
+            <table class="w-full" style="border-spacing: 0 32px; border-collapse: separate">
+                <tbody>
+                    <tr>
+                        <td>
+                            <JetLabel for="email" value="E-mailadres" />
+                        </td>
 
-            <div class="mt-4">
-                <JetLabel for="password" value="Password" />
-                <JetInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-            </div>
+                        <td>
+                            <JetInput
+                                id="email"
+                                v-model="form.email"
+                                type="email"
+                                class="mt-1 block w-full"
+                                required
+                                autofocus
+                            />
+                        </td>
+                    </tr>
 
-            <div class="mt-4">
-                <JetLabel for="password_confirmation" value="Confirm Password" />
-                <JetInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-            </div>
+                    <tr>
+                        <td>
+                            <JetLabel for="password" value="Wachtwoord" />
+                        </td>
+
+                        <td>
+                            <JetInput
+                                id="password"
+                                v-model="form.password"
+                                type="password"
+                                class="mt-1 block w-full"
+                                required
+                                autocomplete="new-password"
+                            />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <JetLabel for="password_confirmation" value="Bevestig wachtwoord" />
+                        </td>
+
+                        <td>
+                            <JetInput
+                                id="password_confirmation"
+                                v-model="form.password_confirmation"
+                                type="password"
+                                class="mt-1 block w-full"
+                                required
+                                autocomplete="new-password"
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
             <div class="flex items-center justify-end mt-4">
-                <JetButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
+                <JetButton>
+                    Wachtwoord veranderen
                 </JetButton>
             </div>
         </form>
-    </JetAuthenticationCard>
+    </div>
 </template>
