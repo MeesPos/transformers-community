41df33ede720d8d8c616267f672c71ba50390063
MeesPos <meespostma_@hotmail.com>
2022-04-19 10:26:56 +0200

    Changed WrongAge page

--- resources/js/Pages/Auth/WrongAge.vue
+++ resources/js/Pages/Auth/WrongAge.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="w-[80%] mx-auto sm:ml-0">
-        <h2 class="text-7xl text-brand-purple font-lalezar" v-show="status === 'too_old'">Helaas u bent te oud</h2>
-        <h2 class="text-7xl text-brand-purple font-lalezar" v-show="status === 'too_young'">Helaas u bent te jong</h2>
+        <h2 class="text-5xl text-brand-purple font-lalezar">Helaas kan je je nu niet aanmelden. </h2>
 
-        <p class="mt-10">Helaas kunt u niet mee doen aan Transformers Community. De geboorte datum dat u bij ons heeft opgeven valt niet ben de juiste leeftijd categorie.  Voor verder informatie kunt u contact met ons opnemen.</p>
+        <p class="mt-10">De community is bedoelt voor jongeren tussen de 16 tot en met 27 jaar. Dit maakt dat jij je op dit moment helaas niet kan aanmelden. We houden bij hoeveel mensen buiten de leeftijdscategorie zich aanmelden. Op het moment dat we deze kunnen uitbreiden, zullen we je hiervan per mail op de hoogte stellen.</p>
 
         <table class="w-full mt-8" style="border-spacing: 0 32px; border-collapse: separate">
             <tbody>
@@ -29,7 +28,7 @@
             </tbody>
         </table>
 
-        <div class="grid grid-cols-2 gap-8 items-center justify-end mt-8">
+        <div class="grid grid-cols-3 gap-8 items-center justify-end mt-8">
             <Link :href="route('login')">
                 <JetButton type="button">
                     Terug
@@ -38,6 +37,10 @@
 
             <JetButton type="button">
                 Help
+            </JetButton>
+
+            <JetButton type="button">
+                Andere Hulporganisatie
             </JetButton>
         </div>
     </div>
