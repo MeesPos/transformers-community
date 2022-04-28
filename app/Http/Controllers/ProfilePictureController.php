7871ffd131849e1d0df968007a6889ebddbd010c
MeesPos <meespostma_@hotmail.com>
2022-04-28 14:45:19 +0200

    Added first start of upload profile picture

--- app/Http/Controllers/ProfilePictureController.php
+++ app/Http/Controllers/ProfilePictureController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ProfilePictureController extends Controller
 {
-    public function index()
+    public function index(): \Inertia\Response
     {
         return Inertia::render('Profile/ProfilePicture');
+    }
+
+    public function delete()
+    {
+        return Auth::user()->update([
+            'profile_photo_path' => null
+        ]);
+    }
+
+    public function upload(Request $request)
+    {
+        // TODO: Upload image
+        dd($request->image);
     }
 }
--- resources/js/Pages/Profile/ProfilePicture.vue
+++ resources/js/Pages/Profile/ProfilePicture.vue
@@ -1,9 +1,37 @@
 <template>
+    <div>
+        <img class="w-48 h-48 rounded-full" :src="$page.props.user.profile_photo_url" />
 
+        <Link method="post" :href="route('profile-picture.delete')">Foto verwijderen</Link>
+
+        <label for="file">Foto uploaden</label>
+        <input type="file" id="file" class="hidden" @input.prevent="upload($event)" accept="image/jpeg, image/png">
+    </div>
 </template>
 
 <script>
+import {Link} from "@inertiajs/inertia-vue3";
 export default {
-    name: "ProfilePicture"
+    name: "ProfilePicture",
+    components: {
+        Link
+    },
+    methods: {
+        upload(event) {
+            let data = new FormData();
+
+            data.append('image', event.target.files[0]);
+
+            console.log(event.target.files[0]);
+
+            axios.post('/profile-picture/upload', data)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+    }
 }
 </script>
