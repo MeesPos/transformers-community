47c39e872089e37055acfb9a2d5e8d8080fd28ca
MeesPos <meespostma_@hotmail.com>
2022-04-29 13:46:55 +0200

    Added profile picture upload

--- resources/js/Pages/Profile/ProfilePicture.vue
+++ resources/js/Pages/Profile/ProfilePicture.vue
@@ -1,36 +1,66 @@
 <template>
     <div>
-        <img class="w-48 h-48 rounded-full" :src="$page.props.user.profile_photo_url" />
+        <img class="w-48 h-48 rounded-full object-cover"
+             :src="$page.props.user.profile_photo_url"
+             ref="profile_picture"
+        />
 
-        <Link method="post" :href="route('profile-picture.delete')">Foto verwijderen</Link>
+        <div @click.prevent="deletePhoto"
+             class="cursor-pointer"
+        >
+            Foto verwijderen
+        </div>
 
-        <label for="file">Foto uploaden</label>
-        <input type="file" id="file" class="hidden" @input.prevent="upload($event)" accept="image/jpeg, image/png">
+        <label for="file"
+               class="cursor-pointer"
+        >
+            Foto uploaden
+        </label>
+        <input ref="profile_picture_input"
+               type="file"
+               id="file"
+               class="hidden"
+               @input.prevent="updateProfileInformation"
+               accept="image/jpeg, image/png"
+        />
     </div>
 </template>
 
 <script>
-import {Link} from "@inertiajs/inertia-vue3";
+import {Link, useForm} from "@inertiajs/inertia-vue3";
+import {Inertia} from "@inertiajs/inertia";
 export default {
     name: "ProfilePicture",
     components: {
         Link
     },
+    data() {
+        return {
+            form: useForm({
+                _method: 'PUT',
+                photo: null
+            })
+        }
+    },
     methods: {
-        upload(event) {
-            let data = new FormData();
+         deletePhoto() {
+            Inertia.delete(this.route('current-user-photo.destroy'), {
+                preserveScroll: true,
+                onSuccess: () => {
+                    this.$refs['profile_picture'].value = null;
+                    this.clearPhotoFileInput();
+                },
+            });
+        },
+        updateProfileInformation() {
+            if (this.$refs['profile_picture_input'].value) {
+                this.form.photo = this.$refs['profile_picture_input'].files[0];
+            }
 
-            data.append('image', event.target.files[0]);
-
-            console.log(event.target.files[0]);
-
-            axios.post('/profile-picture/upload', data)
-                .then(response => {
-                    console.log(response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
+            this.form.post(this.route('user-profile-information.update'), {
+                errorBag: 'updateProfileInformation',
+                preserveScroll: true,
+            });
         }
     }
 }
