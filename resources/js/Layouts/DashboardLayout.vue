2089335ce3e9f803e457c82f9f44f245575108a7
MeesPos <meespostma_@hotmail.com>
2022-05-09 13:21:25 +0200

    Added styling of popup sidebar

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -35,12 +35,59 @@
             </div>
         </div>
 
+        <div class="grid grid-rows-2 text-center shadow-2xl h-screen w-96">
+            <div class="flex flex-col gap-8 ml-8">
+                <div class="mt-10">
+                    <XIcon class="h-auto w-8" />
+                </div>
+
+                <div class="mt-6 flex flex-row gap-6 items-center">
+                    <HomeIcon class="h-auto w-8 text-brand-orange" />
+
+                    <h2 class="text-lg">Home</h2>
+                </div>
+
+                <div class="flex flex-row gap-6 items-center">
+                    <ChatAlt2Icon class="h-auto w-8 text-brand-orange" />
+
+                    <h2 class="text-lg">Berichten</h2>
+                </div>
+
+                <div class="flex flex-row gap-6 items-center">
+                    <UserCircleIcon class="h-auto w-8 text-brand-orange" />
+
+                    <h2 class="text-lg">Account</h2>
+                </div>
+            </div>
+
+            <div class="self-end flex flex-col gap-8 mb-8 ml-8">
+                <div class="flex flex-row gap-6 items-center">
+                    <CogIcon class="h-auto w-8 text-brand-orange" />
+
+                    <h2 class="text-lg">Instellingen</h2>
+                </div>
+
+                <span class="border border-brand-purple px-6 w-12" />
+
+                <div class="flex flex-row gap-6 items-center">
+                    <img class="w-12 h-12 rounded-full"
+                         :src="$page.props.user.profile_photo_url"
+                         :alt="$page.props.user.username"
+                    />
+
+                    <h2 class="text-lg"
+                        v-text="$page.props.user.username"
+                    />
+                </div>
+            </div>
+        </div>
+
         <slot />
     </div>
 </template>
 
 <script>
-import { MenuIcon } from '@heroicons/vue/outline'
+import { MenuIcon, XIcon } from '@heroicons/vue/outline'
 import { UserCircleIcon, CogIcon, HomeIcon, ChatAlt2Icon } from '@heroicons/vue/solid'
 
 export default {
@@ -50,7 +97,8 @@ export default {
         MenuIcon,
         ChatAlt2Icon,
         UserCircleIcon,
-        CogIcon
+        CogIcon,
+        XIcon
     }
 }
 </script>
