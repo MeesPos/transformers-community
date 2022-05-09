52b47c1c9b53d13ce65a3d014bbedfb7fe2a8cce
MeesPos <meespostma_@hotmail.com>
2022-05-09 14:47:32 +0200

    Added toolbox styling

--- resources/js/Pages/Profile/ProfilePicture.vue
+++ resources/js/Pages/Profile/ProfilePicture.vue
@@ -1,45 +1,66 @@
 <template>
-    <div>
-        <img class="w-48 h-48 rounded-full object-cover"
-             :src="$page.props.user.profile_photo_url"
-             ref="profile_picture"
-        />
+    <div class="w-96 border-r-2 border-brand-pink">
+        <div>
+            <h2 class="font-lalezar text-brand-purple text-5xl mt-8 mb-4 pl-8">Profiel</h2>
 
-        <div @click.prevent="deletePhoto"
-             class="cursor-pointer"
-        >
-            Foto verwijderen
+            <div class="flex flex-row items-end justify-center mt-12">
+                <img class="w-48 h-48 rounded-full -z-10 object-cover"
+                     :src="$page.props.user.profile_photo_url"
+                     ref="profile_picture"
+                />
+
+                <div class="cursor-pointer"
+                     :class="editProfilePicture ? '-z-10' : 'z-0'"
+                     @click="editProfilePicture = true"
+                >
+                    <PencilIcon class="w-8 h-8" />
+                </div>
+            </div>
+
+            <div v-show="editProfilePicture" ref="editToolBox" class="bg-[#FCFCFC] w-max p-3 rounded-md shadow-sm z-50 mx-auto mr-6 -mt-16">
+                <div @click.prevent="deletePhoto"
+                     class="cursor-pointer mb-2"
+                >
+                    Foto verwijderen
+                </div>
+
+                <label for="file"
+                       class="cursor-pointer"
+                >
+                    Foto uploaden
+                </label>
+                <input ref="profile_picture_input"
+                       type="file"
+                       id="file"
+                       class="hidden"
+                       @input.prevent="updateProfileInformation"
+                       accept="image/jpeg, image/png"
+                />
+            </div>
         </div>
-
-        <label for="file"
-               class="cursor-pointer"
-        >
-            Foto uploaden
-        </label>
-        <input ref="profile_picture_input"
-               type="file"
-               id="file"
-               class="hidden"
-               @input.prevent="updateProfileInformation"
-               accept="image/jpeg, image/png"
-        />
     </div>
 </template>
 
 <script>
 import {Link, useForm} from "@inertiajs/inertia-vue3";
 import {Inertia} from "@inertiajs/inertia";
+import DashboardLayout from "../../Layouts/DashboardLayout";
+import { PencilIcon } from '@heroicons/vue/solid';
+
 export default {
     name: "ProfilePicture",
     components: {
-        Link
+        Link,
+        PencilIcon
     },
+    layout: DashboardLayout,
     data() {
         return {
             form: useForm({
                 _method: 'PUT',
                 photo: null
-            })
+            }),
+            editProfilePicture: false
         }
     },
     methods: {
