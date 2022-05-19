b087a129d34cf410bb2d3f0cf6386cde0d27371f
MeesPos <meespostma_@hotmail.com>
2022-05-19 14:53:40 +0200

    Added all styling

--- resources/js/Components/ProgressBar.vue
+++ resources/js/Components/ProgressBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="my-16">
+    <nav class="mt-12 mb-8">
         <ol role="list" class="flex items-center justify-center">
-            <li v-for="(step, stepIdx) in steps" :key="step.name" :class="[stepIdx !== steps.length - 1 ? 'pr-8 sm:pr-48' : '', 'relative']">
+            <li v-for="(step, stepIdx) in steps" :key="step.name" :class="[stepIdx !== steps.length - 1 ? 'pr-8 sm:pr-36' : '', 'relative']">
                 <template v-if="step.status === 'complete'">
                     <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                        <div class="h-1 bg-brand-orange" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-8/12' : 'w-full'" />
-                        <div class="h-1 bg-brand-purple" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-4/12' : 'hidden'" />
+                        <div class="h-1 bg-brand-orange" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-7/12' : 'w-full'" />
+                        <div class="h-1 bg-brand-purple" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-5/12' : 'hidden'" />
                     </div>
                     <div class="relative w-10 h-10 flex items-center justify-center bg-brand-orange rounded-full">
                         <span class="text-white font-montserrat font-bold text-xl">{{ step.name }}</span>
--- resources/js/Pages/Onboarding.vue
+++ resources/js/Pages/Onboarding.vue
@@ -2,19 +2,32 @@
     <div class="text-center">
         <img src="images/logo.png"
              alt="Logo Transformers Community"
-             class="w-96 mx-auto my-8"
+             class="w-64 mx-auto my-8"
         />
 
         <div>
             <ProgressBar />
         </div>
 
-        <h1 class="font-lalezar text-7xl text-brand-purple">Over je gevoelens praten</h1>
+        <h1 class="font-lalezar text-6xl text-brand-purple">Over je gevoelens praten</h1>
+
+        <p class="w-[900px] text-left mx-auto text-md mt-4">Welkom bij Transfromers Community.  Leuk dat je heb aangemeld voor onze website en je bij de community wil voegen.  Voordat je kan beginnen leggen we even de basis uit van onze website en hoe je het kan gebruiken voor jou alleen of met elkaar.  Hieronder zie de home pagina van de website, hier kan je alle berichten zien van de community en je eigen berichten plaatsen.  </p>
+
+        <video autoplay="autoplay" width="900" height="400" class="mx-auto mt-4">
+            <source src="/videos/step-2.mp4" type="video/mp4" />
+        </video>
+
+        <div class="grid grid-cols-2 w-[900px] mx-auto mt-8">
+            <button class="bg-brand-aqua font-bold font-montserrat text-lg w-[260px] py-3 rounded-md">Uitleg overslaan</button>
+
+            <button class="bg-brand-aqua font-bold font-montserrat text-lg w-[260px] py-3 place-self-end rounded-md">Volgende</button>
+        </div>
     </div>
 </template>
 
 <script>
 import ProgressBar from "../Components/ProgressBar";
+
 export default {
     name: "Onboarding",
     components: {ProgressBar}
