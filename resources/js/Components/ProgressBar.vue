6dacd1b9a5260c91139d1d651bdefd075ac76db7
MeesPos <meespostma_@hotmail.com>
2022-05-19 14:12:41 +0200

    Added ProgressBar

---
+++ resources/js/Components/ProgressBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="my-16">
+        <ol role="list" class="flex items-center justify-center">
+            <li v-for="(step, stepIdx) in steps" :key="step.name" :class="[stepIdx !== steps.length - 1 ? 'pr-8 sm:pr-48' : '', 'relative']">
+                <template v-if="step.status === 'complete'">
+                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                        <div class="h-1 bg-brand-orange" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-8/12' : 'w-full'" />
+                        <div class="h-1 bg-brand-purple" :class="steps[steps.indexOf(step) + 1].status === 'current' ? 'w-4/12' : 'hidden'" />
+                    </div>
+                    <div class="relative w-10 h-10 flex items-center justify-center bg-brand-orange rounded-full">
+                        <span class="text-white font-montserrat font-bold text-xl">{{ step.name }}</span>
+                    </div>
+                </template>
+                <template v-else>
+                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                        <div class="h-1 w-full bg-brand-purple" />
+                    </div>
+                    <div class="group relative w-10 h-10 flex items-center justify-center bg-white bg-brand-dark-blue rounded-full">
+                        <span class="text-white font-montserrat font-bold text-xl">{{ step.name }}</span>
+                    </div>
+                </template>
+            </li>
+        </ol>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "ProgressBar",
+    data() {
+        return {
+            steps: [
+                { name: '1', href: '#', status: 'complete' },
+                { name: '2', href: '#', status: 'complete' },
+                { name: '3', href: '#', status: 'current' },
+                { name: '4', href: '#', status: 'upcoming' }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Onboarding.vue
+++ resources/js/Pages/Onboarding.vue
@@ -1,17 +1,22 @@
 <template>
-    <div>
+    <div class="text-center">
         <img src="images/logo.png"
              alt="Logo Transformers Community"
+             class="w-96 mx-auto my-8"
         />
+
+        <div>
+            <ProgressBar />
+        </div>
+
+        <h1 class="font-lalezar text-7xl text-brand-purple">Over je gevoelens praten</h1>
     </div>
 </template>
 
 <script>
+import ProgressBar from "../Components/ProgressBar";
 export default {
-    name: "Onboarding"
+    name: "Onboarding",
+    components: {ProgressBar}
 }
 </script>
-
-<style scoped>
-
-</style>
